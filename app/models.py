4bd1137b11661080e24c52375b8ea76e1813548c
agasthyanaibguruprasad <agasthyanaib3@gmail.com>
2025-08-09 15:57:23 +1000

    Enforcing Status Validation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
+from typing import Literal
 
 @dataclass
 class Task:
     title: str
     description: str
-    status: str
+    status: Literal["to-do", "done", "repeat"]
+    
+    def __post_init__(self):
+        allowed_status = ["to-do", "done", "repeat"]
+        if self.status not in allowed_status:
+            raise ValueError("The provided status is not valid.")
