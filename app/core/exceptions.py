edc885e26351373b085e62c8cbe00fe6bc918b30
agasthyanaibguruprasad <agasthyanaib3@gmail.com>
2025-08-10 13:03:09 +1000

    Add File Notfound Exception

---
+++ app/core/exceptions.py
@@ -0,0 +1,6 @@
+class JSONFileNotFound(BaseException):
+    def __init__(self, *args,path):
+        super().__init__(*args)
+        self.path = path
+    def __str__(self):
+        return("The json file is not found in the path: ",self.path)
